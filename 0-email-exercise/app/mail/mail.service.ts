23e896f9a7a886c2cb6da67b613b899ef320c121
svroye <vanroyesteven@gmail.com>
2019-01-10 15:31:15 +0100

    Add mail service

---
+++ 0-email-exercise/app/mail/mail.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+import { Mail } from './models/mail.interface';
+
+
+@Injectable()
+export class MailService {
+
+    constructor(private http: HttpClient){}
+
+    getFolder(folder: string): Observable<Mail[]> {
+        let searchParams = {
+            folder: folder 
+        };
+        return this.http.get<Mail[]>('/api/messages', { params: searchParams });
+    }
+
+}
