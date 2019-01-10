f70479ac514057bb9a4d019f709fc653e43cf1ad
svroye <vanroyesteven@gmail.com>
2019-01-10 15:31:03 +0100

    Subscribing to events

--- 0-email-exercise/app/app.component.ts
+++ 0-email-exercise/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-root',
@@ -28,6 +32,14 @@ import { Router } from '@angular/router';
   `
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router) {}
-  ngOnInit() {}
+  
+  constructor(private router: Router) {
+
+  }
+  
+  ngOnInit() {
+    this.router.events
+    .pipe(filter( event => event instanceof NavigationEnd))
+    .subscribe( event => console.log(event));
+  }
 }
