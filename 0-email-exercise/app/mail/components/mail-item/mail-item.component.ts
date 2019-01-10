61cdc3ade9444544c446ad6807ae6d300be71abe
svroye <vanroyesteven@gmail.com>
2019-01-10 16:19:54 +0100

    Auxiliary navigation with Navigation API

--- 0-email-exercise/app/mail/components/mail-item/mail-item.component.ts
+++ 0-email-exercise/app/mail/components/mail-item/mail-item.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, Input } from '@angular/core';
 
 import { Mail } from '../../models/mail.interface';
@@ -6,8 +7,10 @@ import { Mail } from '../../models/mail.interface';
   selector: 'mail-item',
   styleUrls: ['mail-item.component.scss'],
   template: `
-    <a class="mail-item"
-      [routerLink]="['', { outlets: { pane: ['message', message.id] } } ]" routerLinkActive="active">
+  <!-- routing from the the template -->
+  <!-- <a class="mail-item"
+      [routerLink]="['', { outlets: { pane: ['message', message.id] } } ]" routerLinkActive="active"> -->
+    <a class="mail-item" (click)="navigateToMessage()">
       <h3>
         {{ message.from }}
         <span>{{ message.timestamp | date:'shortTime' }}</span>
@@ -19,5 +22,11 @@ import { Mail } from '../../models/mail.interface';
 export class MailItemComponent {
   @Input()
   message: Mail;
+
+  constructor(private router: Router){}
+
+  navigateToMessage(){
+    this.router.navigate( ['', { outlets: { pane: ['message', this.message.id] } } ])
+  }
 
 }
