5f73c1b1eefee38b586f26633da524ba15280011
svroye <vanroyesteven@gmail.com>
2019-01-10 10:45:13 +0100

    Initial commit

---
+++ 0-email-exercise/app/app.module.ts
@@ -0,0 +1,28 @@
+import { MailModule } from './mail/mail.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+
+import { AppComponent } from './app.component';
+
+export const ROUTES: Routes = [
+  { path: '**', redirectTo: 'folder/inbox' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    MailModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {}
