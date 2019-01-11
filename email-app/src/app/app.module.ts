ad8f10550fe6a3627b1d65ddc2f3c7a6f8ca863e
svroye <vanroyesteven@gmail.com>
2019-01-11 14:19:00 +0100

    Add a custom preloader

--- email-app/src/app/app.module.ts
+++ email-app/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { MailModule } from './mail/mail.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules, PreloadingStrategy, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DashboardModule } from './dashboard/dashboard.module';
+import { Observable, of } from 'rxjs';
 
 export const ROUTES: Routes = [
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+  { path: 'dashboard', data: { preload: true }, loadChildren: './dashboard/dashboard.module#DashboardModule' },
   { path: '**', redirectTo: 'mail/folder/inbox' }
 ];
+
+
+export class CustomPreload implements PreloadingStrategy {
+
+  preload(route: Route,  fn: () => Observable<any>): Observable<any> {
+    return route.data && route.data.preload ? fn() : of(null);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -22,10 +30,15 @@ export const ROUTES: Routes = [
     MailModule,
     // for route tracing
     //RouterModule.forRoot(ROUTES, { enableTracing: true })
-    RouterModule.forRoot(ROUTES)
+    // use the preloadingStrategy PreloadAllModules for preloading all modules
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: CustomPreload})
+  ],
+  providers: [
+    CustomPreload
   ],
   bootstrap: [
     AppComponent
   ]
 })
 export class AppModule {}
+ 
