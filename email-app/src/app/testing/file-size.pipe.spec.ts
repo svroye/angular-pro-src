71bd16263922c327962890a638dda7f6a61ab873
Steven Van Roye <svr1m2f@admin.colruyt.int>
2019-01-14 09:29:09 +0100

    Add unit tests with the Angular framework

--- email-app/src/app/testing/file-size.pipe.spec.ts
+++ email-app/src/app/testing/file-size.pipe.spec.ts
@@ -1,9 +1,61 @@
 import { FileSizePipe } from "./file-size.pipe";
 
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { Component } from "@angular/core";
+
 
 describe('FileSizePipe', () => {
     
- 
+  describe('Shallow FileSizePipe test', () => {
+
+    @Component({
+      template: `
+        Size: {{ size | filesize: suffix }}
+      `
+    })
+    class TestComponent {
+      suffix;
+      size = 123456789;
+    }
+
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          FileSizePipe,
+          TestComponent
+        ]
+      });
+
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+    });
+
+    it('should convert bytes to megabytes', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+      component.size = 1029281;
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 0.98MB');
+    });
+
+    it('should use the default extension when not supplied', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+    });
+
+    it('should override the extension when supplied', () => {
+      component.suffix = 'myExt';
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74myExt');
+    });
+
+
+  });
 
 
   describe('Isolated FileSizePipe test', () => {
