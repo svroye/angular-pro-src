f45ac16d213fc0305b41fce917343a671e98f885
svroye <vanroyesteven@gmail.com>
2019-01-15 15:10:24 +0100

    zones and ngZone

--- dependency-injection-and-zones-app/src/app/app.component.ts
+++ dependency-injection-and-zones-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, NgZone } from '@angular/core';
 import { FoodStoreService } from './food-store/food-store.service';
 
 @Component({
@@ -14,19 +14,39 @@ import { FoodStoreService } from './food-store/food-store.service';
   `],
   template: `
     <div>
+    <!--
       <pizza-viewer></pizza-viewer>
       <side-viewer></side-viewer>
-      <drink-viewer></drink-viewer>
+      <drink-viewer></drink-viewer>        
       <div>
         Food Store : {{ (store | async)?.name }}
+      </div>-->
+      <div>
+        Counter: {{ counter }}
       </div>
     </div>
   `
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck{
 
   store = this.foodService.getStore();
 
-  constructor(private foodService: FoodStoreService) {}
+  constructor(private foodService: FoodStoreService, private zone: NgZone) {}
 
+  counter = 0;
+
+  ngOnInit() {
+    this.zone.runOutsideAngular( () => {
+      for (let i = 0; i < 100; i++){
+        setTimeout( () => this.counter++);
+      }
+      this.zone.run( () => {
+        setTimeout( () => this.counter = this.counter, 1000);
+      });
+    });
+  }
+
+  ngDoCheck(){
+    console.log('Change detection');
+  }
 }
